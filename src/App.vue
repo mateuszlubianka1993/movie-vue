80c7cafa65db4ac9a72f5b65cbcd3ea4b74c5c32
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-06-02 13:13:51 +0200

    Add Header component.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header/Header.vue'
 
 export default {
   name: 'App',
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-header">
+    <div class="header-content">
+      <h1>Movie App Vue</h1>
+      <p>{{ msg }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.main-header {
+  background-color: aqua;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
