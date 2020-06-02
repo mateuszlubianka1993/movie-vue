74fe018fd5bcdcda91f6d2571c0653e9d5f271ef
Mateusz Łubianka <lubiankamateusz@gmail.com>
2020-06-02 14:35:03 +0200

    Add config for axios and getPopularMovies function.

--- src/components/Home/HomePage.vue
+++ src/components/Home/HomePage.vue
@@ -7,11 +7,39 @@
 
 <script>
 import Header from '../Header/Header';
+import tmdb from '../../apis/tmdb';
+
+const apiKey = '3e47509c5e108f3c61f81a43fdd0bb7c';
 
 export default {
   name: 'HomePage',
+  data(){
+    return {
+      popularMovies: []
+    }
+  },
   components: {
     Header
+  },
+  methods: {
+    getPopularMovies() {
+      tmdb.get('/movie/popular', {
+        params: {
+          api_key: apiKey
+        }
+      })
+      .then(response => {
+        const result = response.data.results;
+        this.popularMovies = result;
+        console.log(this.popularMovies)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  },
+  created() {
+    this.getPopularMovies();
   }
 }
 </script>
